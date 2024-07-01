a27226fd04af496c02b3326d0005a383060a19b1
Benjamin Schäfer <benibschafer@gmail.com>
2024-07-01 08:43:44 +0000

    chore: prepare for deployment on Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gipfel_tutor.wsgi:application
--- gipfel_tutor/settings.py
+++ gipfel_tutor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -107,12 +108,17 @@ WSGI_APPLICATION = 'gipfel_tutor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
