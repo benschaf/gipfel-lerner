7d240f9afeeb68934e4a3678c87660c674658e91
Benjamin Schäfer <benibschafer@gmail.com>
2024-07-01 13:40:16 +0000

    feat: increase cache expiry date for staticfiles

--- gipfel_tutor/settings.py
+++ gipfel_tutor/settings.py
@@ -169,6 +169,12 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if 'USE_AWS' in os.environ:
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'gipfel-tutor-768a610dc54f'
     AWS_S3_REGION_NAME = 'eu-north-1'
