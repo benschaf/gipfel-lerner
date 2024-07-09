39a96d0605ac088dacd1f97eb6031f4c0752eb14
Benjamin Schäfer <benibschafer@gmail.com>
2024-07-09 12:12:45 +0000

    feat: add booking models to admin.py

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from booking.models import Payment, TutoringSession
+
 # Register your models here.
+admin.site.register(TutoringSession)
+admin.site.register(Payment)
+
