c7a4f866df9ef0f3b3c48e3d4d23e9460c588b6f
Benjamin Schäfer <benibschafer@gmail.com>
2024-08-07 12:33:55 +0000

    doc: remove old sass documentation

--- documentation/additional_credits.js
+++
@@ -1,7 +0,0 @@
-/**
- * This file is only used to credit sources that are not
- * directly related to any of the files in the repository.
- */
-
-// -> Credit for using Bootstrap with SASS: https://www.youtube.com/watch?v=nCX3QVl_PiI&list=PL4cUxeGkcC9joIM91nLzd_qaH_AimmdAR&index=19&ab_channel=NetNinja
-// -> Credit for SASS compile setup: https://sprucecss.com/blog/the-simplest-sass-compile-setup/
