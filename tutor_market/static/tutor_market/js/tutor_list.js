f1e26b650451df29d06244f060ed95a1dac5f1cf
Benjamin Schäfer <benibschafer@gmail.com>
2024-08-07 13:08:50 +0000

    refac: extract tutor_list.html scripts

---
+++ tutor_market/static/tutor_market/js/tutor_list.js
@@ -0,0 +1,7 @@
+const radio_btns = document.querySelectorAll('.btn-check');
+const form = document.querySelector('form');
+radio_btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        form.submit();
+    });
+});
