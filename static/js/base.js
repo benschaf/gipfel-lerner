b48afae09ad5613f353ffb2f907ea6f98445b3ef
Benjamin Schäfer <benibschafer@gmail.com>
2024-09-04 10:33:21 +0000

    refac: change base.js to work without jquery
    this makes use with jest easier

--- static/js/base.js
+++ static/js/base.js
@@ -2,20 +2,28 @@
  * Updates the copyright notice on the webpage.
  */
 function updateCopyrightNotice() {
-    let getYear = new Date().getFullYear()
-    let yearID = $('#year')[0]
+    let getYear = new Date().getFullYear();
+    let yearID = document.getElementById('year');
     if (getYear == 2024) {
-        yearID.innerHTML = getYear
+        yearID.innerHTML = getYear;
     } else {
-        yearID.innerHTML = `2024 - ${getYear}`
+        yearID.innerHTML = `2024 - ${getYear}`;
     }
 }
 
-$(document).ready(function () {
-    $('.toast').toast('show')
+document.addEventListener("DOMContentLoaded", function() {
+    var toastElements = document.querySelectorAll('.toast');
+    toastElements.forEach(function(toastElement) {
+        var toast = new bootstrap.Toast(toastElement);
+        toast.show();
+    });
 
-    updateCopyrightNotice()
+    updateCopyrightNotice();
 });
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+if (typeof module !== "undefined") module.exports = {
+    updateCopyrightNotice
+};
