8885aa8f58902bb6e6ef6733d4847aea799920fc
Benjamin Schäfer <benibschafer@gmail.com>
2024-09-12 06:16:40 +0000

    chore: add development database option to settings

--- gipfel_tutor/settings.py
+++ gipfel_tutor/settings.py
@@ -171,6 +171,13 @@ elif "DATABASE_URL" in os.environ:
     DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
